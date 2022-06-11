aca77869bc2bacdb15befc2781aeef5171388be7
Alfafis <ctpmmg2009_101@hotmail.com>
2022-06-11 03:05:12 -0300

    fix: up schema postgresql

--- assets/UpdateFromSql.py
+++ assets/UpdateFromSql.py
@@ -7,7 +7,7 @@ import sys
 
 from pandas.io.json import json_normalize
 from numpy import array
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, true
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,47 +26,59 @@ record_list.to_json(r'assets/Relatorio_cadop.json')
 
 with open('assets/Relatorio_cadop.json', 'w', encoding='utf-8') as file:
     record_list.to_json(file, force_ascii=False)
+
     list_col = list(record_list.columns)
-    col = array(list_col)
-    print("teste -> ", col)
+    col = str(list_col)
+    col_paramns = col.replace(" ", "_").replace("'", "").replace(",_", " text, ").replace("[", "").replace("]", " text").replace("ANS text", "ANS integer").replace("CNPJ text", "CNPJ integer unique not null").replace(
+        "ANS integer, CNPJ", "ANS integer primary key, CNPJ").replace("Número text", "Numero integer").replace("CEP text", "CEP integer").replace("DDD text", "DDD integer").replace("Telefone text", "Telefone integer").replace("Fax text", "Fax integer").replace("Endereço_eletrônico", "Email")
 
-# def create_table(json_data):
-#     try:
-#         with psycopg2.connect(DATABASE_URL) as conn:
-#             cur = conn.cursor()
-#             print("connect")
-#             cur.execute("CREATE TABLE IF NOT EXISTS Relatorio " +
-#                         (str(list_col)))
-#             print("create")
-#             conn.commit()
-#             conn.close()
-#             print("Done")
-#     except(Exception, psycopg2.Error) as error:
-#         print("Failed json import: {}".format(error))
+    value_paramns = str(record_list.head(3))
+    print(value_paramns)
 
 
-# create_table(col)
+def create_table(json_data):
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+    except(Exception, psycopg2.Error) as error:
+        print("Connection not established", error)
 
-# engine = create_engine("postgresql+psycopg2://thisuser:"+DATABASE_URL)
-# col.to_sql("new_table", engine, index=False, if_exists='append')
-# print("Done").str.split(pat="\n", expand=True)
+    cur = conn.cursor()
+    print("connect")
+
+    cur.execute(
+        "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name='relatorio')")
+    table_exist = cur.fetchone()[0]
+    print(table_exist)
+
+    if bool(table_exist):
+        cur.execute("CREATE TABLE IF NOT EXISTS relatorio (" +
+                    col_paramns + ")")
+        conn.commit()
+        print("create")
+        conn.close()
+        print("Done")
+    else:
+        conn.close()
+        print('Relatorio table exists. Moving On.')
 
 
-# def insert_into_table(json_data):
-#     try:
-#         with psycopg2.connect(DATABASE_URL) as conn:
-#             cur = conn.cursor()
-#             lines_json_all = data_base.readlines()
-#             query = "INSERT INTO new_table VALUES (%s)"
-#             cur.executemany(query, (lines_json_all))
-#             conn.commit()
-#             conn.close()
-#     except(Exception, psycopg2.Error) as error:
-#         print("Failed json import: {}".format(error))
+# create_table(col_paramns)
+
+def insert_into_table(json_data):
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+    except(Exception, psycopg2.Error) as error:
+        print("Connection not established", error)
+
+    cur = conn.cursor()
+    print("connect")
+
+    query = "INSERT INTO relatorio VALUES (%s)"
+    cur.executemany(query, value_paramns)
+    conn.commit()
+    print("commit")
+    conn.close()
+    print("close")
 
 
-# insert_into_table(data_base)
-
-# create_table()
-
-# df = pd.json_normalize(data_base)
+insert_into_table(value_paramns)
